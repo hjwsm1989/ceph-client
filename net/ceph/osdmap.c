4922cedf69a8f5f1290b42f9dbd53da461f77b17
Sage Weil <sage@newdream.net>
2011-09-04 20:39:22 -0700

    libceph: fail more gracefully if crush_do_rule returns an error
    Signed-off-by: Sage Weil <sage@newdream.net>

--- net/ceph/osdmap.c
+++ net/ceph/osdmap.c
@@ -1048,6 +1048,7 @@ static int *calc_pg_raw(struct ceph_osdmap *osdmap, struct ceph_pg pgid,
 	int ruleno;
 	unsigned poolid, ps, pps;
 	int preferred;
+	int r;
 
 	/* pg_temp? */
 	pg = __lookup_pg_mapping(&osdmap->pg_temp, pgid);
@@ -1087,9 +1088,16 @@ static int *calc_pg_raw(struct ceph_osdmap *osdmap, struct ceph_pg pgid,
 				      le32_to_cpu(pool->v.pgp_num),
 				      pool->pgp_num_mask);
 	pps += poolid;
-	*num = crush_do_rule(osdmap->crush, ruleno, pps, osds,
-			     min_t(int, pool->v.size, *num),
-			     preferred, osdmap->osd_weight);
+	r = crush_do_rule(osdmap->crush, ruleno, pps, osds,
+			  min_t(int, pool->v.size, *num),
+			  preferred, osdmap->osd_weight);
+	if (r < 0) {
+		pr_err("error %d from crush rule pool %d ruleset %d type %d"
+		       " size %d\n", poolid, pool->v.crush_ruleset,
+		       pool->v.type, pool->v.size);
+		return NULL;
+	}
+	*num = r;
 	return osds;
 }
 
